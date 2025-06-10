3160709248be7f139f811f2eeb83db092fc1f782
fverse <mail.fawazabdulla@gmail.com>
2025-06-10 10:21:24 +0530

    take file as a command line argument

--- bin/main.ml
+++ bin/main.ml
@@ -1,44 +1,53 @@
 open Lwt.Syntax
+open Cmdliner
 
-let exec_processes () =
-  (* TODO: take file as a command line argument *)
-  let proc_defs = Raika.load_procfile "Procfile" in
-  let (waiter, wakener) = Lwt.wait () in
+let file_arg =
+  let doc = "The path to the Procfile to execute." in
+  let info = Arg.info [ "f"; "file" ] ~docv:"PATH" ~doc in
 
-  if proc_defs = [] then
-    begin
+  Arg.value (Arg.opt Arg.string "Procfile" info)
+
+let exec_processes filepath =
+  (Lwt.async_exception_hook :=
+     fun exn ->
+       Printf.eprintf "Unhandled Lwt exception: %s\n" (Printexc.to_string exn));
+  let main () =
+    let proc_defs = Raika.load_procfile filepath in
+    let waiter, wakener = Lwt.wait () in
+
+    if proc_defs = [] then (
       print_endline "Procfile is empty or not found";
-      Lwt.return_unit
-    end
-  else
-    begin
+      Lwt.return_unit)
+    else
       let processes = ref [] in
       let process_statuses = ref [] in
 
-      let _ = Lwt_unix.on_signal Sys.sigint (fun _ ->
-        prerr_endline "\nCaught Ctrl+C, shutting down all processes...";
+      let _ =
+        Lwt_unix.on_signal Sys.sigint (fun _ ->
+            prerr_endline "\nCtrl+C, shutting down all processes...";
 
-         (* Iterate over the processes list
+            (* Iterate over the processes list
          and call the 'terminate' method on each process *)
-         List.iter (fun proc ->
-           proc#terminate
-         ) !processes;
-        Lwt.wakeup wakener ();
-      ) in
+            List.iter (fun proc -> proc#terminate) !processes;
+            Lwt.wakeup wakener ())
+      in
 
-      List.iter (fun proc_def ->
-        let (process, status) = Raika.exec_proc proc_def in
-        processes := process :: !processes;
-        process_statuses := status :: !process_statuses;
-      ) proc_defs;
+      List.iter
+        (fun proc_def ->
+          let process, status = Raika.exec_proc proc_def in
+          processes := process :: !processes;
+          process_statuses := status :: !process_statuses)
+        proc_defs;
 
-      let* () = Lwt.pick [Lwt.join !process_statuses; waiter] in
+      let* () = Lwt.pick [ Lwt.join !process_statuses; waiter ] in
       Lwt.return_unit
-    end
+  in
+  Lwt_main.run (main ())
 
-let () =
-  Lwt.async_exception_hook := (fun exn ->
-    Printf.eprintf "Unhandled Lwt exception: %s\n" (Printexc.to_string exn)
-  );
-  
-  Lwt_main.run (exec_processes ())
+let main_term = Term.(const exec_processes $ file_arg)
+
+let cmd_info =
+  Cmd.info "raika" ~version:"0.1.0" ~doc:"Procfile/Yaml based process manager."
+
+let cmd = Cmd.v cmd_info main_term
+let () = exit (Cmd.eval cmd)
--- lib/raika.mli
+++ lib/raika.mli
@@ -1,8 +1,4 @@
+type process = { proc_name : string; command : string }
 
-type process = {
-  proc_name    : string;
-  command : string;
-}
 val load_procfile : string -> process list
-
-val exec_proc : process -> (Lwt_process.process_full * unit Lwt.t)
+val exec_proc : process -> Lwt_process.process_full * unit Lwt.t
